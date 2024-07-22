1392cb8695ce30a4843b0c435e57cfecd3114bb5
vishnu-MVP <92323724+vishnu-MVP@users.noreply.github.com>
2024-07-22 18:09:35 +0530

    removed paddlespeech

--- Backend/libraries_used.py
+++ Backend/libraries_used.py
@@ -1,6 +1,5 @@
 from deep_translator import GoogleTranslator
-from paddlespeech.cli.asr.infer import ASRExecutor
-from paddlespeech.cli.tts.infer import TTSExecutor
+
 import detectlanguage
 
 
@@ -12,10 +11,7 @@ langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
 
 my_api_key = "3bb1cba98da8d5fef65f47e7142f1baf"
 detectlanguage.configuration.api_key = my_api_key
-asr = ASRExecutor()
-#result = asr(audio_file="C:\\Users\\vishnu\\Documents\\Sound recordings\\Audio_STT.m4A")
-tts = TTSExecutor()
-tts(text="今天天气十分不错。", output="output.wav")
+
 var=detectlanguage.detect("Buenos dias señor")
 print(var)
 
